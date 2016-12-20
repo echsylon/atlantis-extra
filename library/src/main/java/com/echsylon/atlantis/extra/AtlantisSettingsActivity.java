a844a6d87fb135e388be0bf78ef1ecbdaeabaf27
László Urszuly <laszlo.urszuly@jayway.com>
2016-12-20 19:18:23 +0100

    move fixup

--- library/src/main/java/com/echsylon/atlantis/extra/AtlantisSettingsActivity.java
+++ library/src/main/java/com/echsylon/atlantis/extra/AtlantisSettingsActivity.java
@@ -1,14 +1,17 @@
 package com.echsylon.atlantis.extra;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
 public class AtlantisSettingsActivity extends PreferenceActivity {
 
     @Override
-    @SuppressWarnings("ConstantConditions") // ignore Lint getSupportActionBar()
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = new Intent(getApplicationContext(), AtlantisService.class);
+        startService(intent);
+
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new AtlantisSettingsFragment())
                 .commit();
